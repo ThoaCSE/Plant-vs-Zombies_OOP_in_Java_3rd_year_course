7dfe4638fdfaf0f04e08a0c39c0f51029fb7091a
ThoaCSE <nguyenhuynhkthoa@gmail.com>
2025-10-29 11:09:49 +0700

    delete the old file and built the game structure again. Add package main and add the game class

---
+++ scr/main/Game.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
+    public Game () {
+        setSize(400,400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args){
+        System.out.println("Hello");
+        Game game = new Game();
+    }
+}
