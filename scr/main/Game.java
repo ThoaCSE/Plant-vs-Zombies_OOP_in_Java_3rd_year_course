c7a92635ecafeb7f31696b66a5fc42c8c1064f5f
ThoaCSE <nguyenhuynhkthoa@gmail.com>
2025-10-29 11:25:15 +0700

    Add GameScreen file for the game

--- scr/main/Game.java
+++ scr/main/Game.java
@@ -3,12 +3,16 @@ package main;
 import javax.swing.*;
 
 public class Game extends JFrame {
+    public GameScreen gameScreen;
+
     public Game () {
         setSize(400,400);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-    }
+        gameScreen = new GameScreen();
+        add(gameScreen);
+   }
     public static void main(String[] args){
         System.out.println("Hello");
         Game game = new Game();
